cf1dc6501a894acb99a4e5d95119c8f90ac52f71
ChaosValravn25 <silver.wolf1@hotmail.com>
2025-08-19 00:03:10 -0400

    cambios en main.dart

--- lab/lib/main.dart
+++ lab/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:logger/logger.dart';
 void main() {
+  var logger = Logger();
+  logger.d("Logger iniciado correctamente");
+
   runApp(const MyApp());
 }
 
